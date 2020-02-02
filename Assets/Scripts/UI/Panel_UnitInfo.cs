150ddcd09e48fba7e7ce04a0080a2f18b9048cd1
Chanmob <sellingout@naver.com>
2020-02-02 21:59:07 +0900

    유닛 정보창 추가

---
+++ Assets/Scripts/UI/Panel_UnitInfo.cs
@@ -0,0 +1,33 @@
+﻿/*
+  ============================================
+	Author	: ChanMob
+	Time 	: 2020-02-02 오후 9:52:37
+  ============================================
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Panel_UnitInfo : UI_Base
+{
+    /* [PUBLIC VARIABLE]					*/
+
+
+    /* [PROTECTED && PRIVATE VARIABLE]		*/
+
+    [SerializeField] private Image _image_UnitImage;
+
+    [SerializeField] private Text _text_UnitName;
+    [SerializeField] private Text _text_Level;
+    [SerializeField] private Text _text_Power;
+    [SerializeField] private Text _text_Speed;
+
+    /*----------------[PUBLIC METHOD]------------------------------*/
+
+
+    /*----------------[PROTECTED && PRIVATE METHOD]----------------*/
+
+
+}
