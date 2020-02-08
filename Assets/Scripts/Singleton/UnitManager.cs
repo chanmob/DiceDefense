0998a54f45f23a4dae2eb9b743d7ff74d5d4fbce
chanmob <sellingout@naver.com>
2020-02-08 18:36:44 +0900

    유닛 구입시 속성있으면 효과 추가

--- Assets/Scripts/Singleton/UnitManager.cs
+++ Assets/Scripts/Singleton/UnitManager.cs
@@ -120,24 +120,42 @@ public class UnitManager : Singleton<UnitManager>
         {
             case 0:
                 if(_ingameManager.unitOneFirstAttribute != DataDefine.Attribute.None)
+                {
                     unitObject.firstAttribue = _ingameManager.unitOneFirstAttribute;
+                    unitObject.SetFirstAttribute();
+                }
 
-                if(_ingameManager.unitOneSecondAttribute != DataDefine.Attribute.None)
+                if (_ingameManager.unitOneSecondAttribute != DataDefine.Attribute.None)
+                {
                     unitObject.secondAttribue = _ingameManager.unitOneSecondAttribute;
+                    unitObject.SetSecondAttribute();
+                }
                 break;
             case 1:
                 if (_ingameManager.unitTwoFirstAttribute != DataDefine.Attribute.None)
+                {
                     unitObject.firstAttribue = _ingameManager.unitTwoFirstAttribute;
+                    unitObject.SetFirstAttribute();
+                }
 
                 if (_ingameManager.unitTwoSecondAttribute != DataDefine.Attribute.None)
+                {
                     unitObject.secondAttribue = _ingameManager.unitTwoSecondAttribute;
+                    unitObject.SetSecondAttribute();
+                }
                 break;
             case 2:
                 if (_ingameManager.unitThreeFirstAttribute != DataDefine.Attribute.None)
+                {
                     unitObject.firstAttribue = _ingameManager.unitThreeFirstAttribute;
+                    unitObject.SetFirstAttribute();
+                }
 
                 if (_ingameManager.unitThreeSecondAttribute != DataDefine.Attribute.None)
+                {
                     unitObject.secondAttribue = _ingameManager.unitThreeSecondAttribute;
+                    unitObject.SetSecondAttribute();
+                }
                 break;
         }
 
