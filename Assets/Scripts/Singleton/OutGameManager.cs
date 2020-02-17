04600b7e473a8cf18339b9fae29ab637fcb83971
Chanmob <sellingout@naver.com>
2020-02-17 14:35:41 +0900

    사운드 추가

---
+++ Assets/Scripts/Singleton/OutGameManager.cs
@@ -0,0 +1,26 @@
+﻿/*
+  ============================================
+	Author	: ChanMob
+	Time 	: 2020-02-17 오후 2:34:37
+  ============================================
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutGameManager : Singleton<OutGameManager>
+{
+    /* [PUBLIC VARIABLE]					*/
+
+
+    /* [PROTECTED && PRIVATE VARIABLE]		*/
+
+
+    /*----------------[PUBLIC METHOD]------------------------------*/
+
+
+    /*----------------[PROTECTED && PRIVATE METHOD]----------------*/
+
+
+}
